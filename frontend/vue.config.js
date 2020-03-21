c335f5f268efb15f5dcc9a94ee6dcb4c68bc9333
Eduardo Zima <eduardo.zima@fullcontrol.com>
2020-03-21 18:41:39 +0100

    fixed production build path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 
 // vue.config.js
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                _assets_: path.resolve(__dirname, '/src/assets')
-            }
-        }
+  publicPath: './',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        _assets_: path.resolve(__dirname, '/src/assets')
+      }
     }
   }
-  
+}
