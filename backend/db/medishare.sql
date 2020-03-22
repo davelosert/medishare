9e0e8d7f65dfbf111a368b079c88faaf441d2a03
hjboehle <hj@boehle.info>
2020-03-22 13:58:27 +0100

    new approach to create db

---
+++ backend/db/medishare.sql
@@ -0,0 +1,179 @@
+CREATE DATABASE  IF NOT EXISTS `medishare` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `medishare`;
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: medishare
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `advertisement_categories`
+--
+
+DROP TABLE IF EXISTS `advertisement_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `advertisement_categories` (
+  `advertisement_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `advertisement_category_name` varchar(5) NOT NULL,
+  PRIMARY KEY (`advertisement_category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `advertisement_categories`
+--
+
+LOCK TABLES `advertisement_categories` WRITE;
+/*!40000 ALTER TABLE `advertisement_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `advertisement_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `advertisement_status`
+--
+
+DROP TABLE IF EXISTS `advertisement_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `advertisement_status` (
+  `advertisement_status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `advertisement_status_name` int(11) NOT NULL,
+  PRIMARY KEY (`advertisement_status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `advertisement_status`
+--
+
+LOCK TABLES `advertisement_status` WRITE;
+/*!40000 ALTER TABLE `advertisement_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `advertisement_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `advertisements`
+--
+
+DROP TABLE IF EXISTS `advertisements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `advertisements` (
+  `advertisement_id` int(11) NOT NULL AUTO_INCREMENT,
+  `advertisement_topic` varchar(100) NOT NULL,
+  `advertisement_content` text NOT NULL,
+  `number_of_items` int(11) NOT NULL,
+  `desired_date` datetime NOT NULL,
+  `advertisement_category_id` int(11) NOT NULL,
+  `advertisement_status_id` int(11) NOT NULL,
+  PRIMARY KEY (`advertisement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `advertisements`
+--
+
+LOCK TABLES `advertisements` WRITE;
+/*!40000 ALTER TABLE `advertisements` DISABLE KEYS */;
+/*!40000 ALTER TABLE `advertisements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `plz` char(5) NOT NULL,
+  `place_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medical_practices`
+--
+
+DROP TABLE IF EXISTS `medical_practices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medical_practices` (
+  `medical_practice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lanr` char(9) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY (`medical_practice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medical_practices`
+--
+
+LOCK TABLES `medical_practices` WRITE;
+/*!40000 ALTER TABLE `medical_practices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medical_practices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `mail_address` varchar(50) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `medical_practice_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-22 13:06:27
