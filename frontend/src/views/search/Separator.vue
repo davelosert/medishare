ff3f477266dff5326e788b2c5cceb73c4e64f0ae
Eduardo Zima <eduardo.zima@fullcontrol.com>
2020-03-22 13:10:38 +0100

    created separator vue

---
+++ frontend/src/views/search/Separator.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-col 
+      class="d-flex justify-content-around align-items-center w-100 separator-container p-0 flex-grow-0">
+      <div class="separator flex-grow-1"></div>
+      <span class="delimiter">{{ delimiter }}</span>
+      <div class="separator flex-grow-1"></div>
+    </b-col>
+</template>
+
+<script>
+export default {
+  name: 'separator',
+  props: {
+    delimiter: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.separator {
+  height: 1px;
+  background-color: #dddddd;
+}
+
+.separator-container {
+  margin: 15px 0px;
+}
+
+.delimiter {
+  line-height: 1.5;
+  text-align: center;
+  color: #dddddd;
+  margin: 0 8px;
+}
+</style>
