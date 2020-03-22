1a877f411b84f5a28d20227f702c89e73917c37b
David Losert <david.losert@gmx.de>
2020-03-22 22:29:51 +0100

    Adjusted categories to actually call backend api

--- frontend/src/api/main/categories.js
+++ frontend/src/api/main/categories.js
@@ -1,30 +1,11 @@
 export default {
-  fetchCategories () {
-    return Promise.resolve([{
-      id: 1,
-      name: 'OP-Masken',
-      image: 'illustration-op-mask.svg'
-    },{
-      id: 2,
-      name: 'FFP2 / FFP3 Schutzmasken',
-      image: 'illustration-ffp-mask.svg'
-    },{
-      id: 3,
-      name: 'Handschuhe',
-      image: 'illustration-handschuh.svg'
-    },{
-      id: 4,
-      name: 'Schutzbrillen',
-      image: 'illustration-schutzbrille.svg'
-    },{
-      id: 5,
-      name: 'Desinfektionsmittel',
-      image: 'illustration-desinfektionsmittel.svg'
-    },{
-      id: 6,
-      name: 'Schutzkleidung',
-      image: 'illustration-schutzkleidung.svg'
-    }
-  ])
+  fetchCategories() {
+    return fetch('/api/categories/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      });
   }
 }
